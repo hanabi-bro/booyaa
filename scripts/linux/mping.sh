97fd0fe3749e786d0589a4adad32c2e905b03e9f
hanabi-bro <hanabi.bro@gmail.com>
2026-04-08 15:30:12 +0900

    traffic_teseter beta1 mod script

--- scripts/linux/mping.sh
+++ scripts/linux/mping.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set +o histexpand
 
 PYTHON="$HOME/.local/booyaa/.venv/bin/python"
-script="$HOME/.local/booyaa/mping.py"
+script="$HOME/.local/booyaa/booyaa/mping/mping.py"
 
 exec "$PYTHON" "$script" "$@"
 
